1abe5e4d62184e2a5cc0a9bec8c6276ed5bb71c9
Dylan Rizo <you@example.com>
2023-05-12 19:07:42 -0600

    funcies de grupos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, session
-from flask_socketio import SocketIO, send, emit, join_room
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_session import Session
 
 app = Flask(__name__)
@@ -7,29 +7,67 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = 'mysecretkey'
 socketio = SocketIO(app, cors_allowed_origins="*")
 chat_rooms ={}
+messages = {}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def nombreusuario():
     if 'username' in session:
-        return render_template('index.html')
+        return render_template('index.html', chat_rooms=chat_rooms)
     if request.method == 'POST':
         username = request.form.get("username")
+        print(username)
         session['username'] = username
-        return render_template('index.html')
+        print(session['username'])
+        return render_template('index.html', chat_rooms=chat_rooms)
     return render_template('nickname.html')
 
 
 @socketio.on("message")
 def message(data):
-    emit("message", data['message'], room=data['room'], broadcast=False, include_self=True)
+    room = data['room']
+    message = {
+        'username': session['username'],
+        'text': data['message'] 
+        }
+    if room not in messages:
+        messages[room] = []
+    messages[room].append(message)
+    # Limit the number of messages to 100
+    if len(messages[room]) > 100:
+        messages[room] = messages[room][-100:]
+    emit("message", message, room=room, broadcast=False, include_self=True)
+
 
 @socketio.on("create_room")
 def create_room(room_name):
+    chat_rooms[room_name] = []
     join_room(room_name)
+    emit("room_list", list(chat_rooms.keys()), broadcast=True)
     emit("message", f"Te has unido a la sala {room_name}.", room=room_name)
     return room_name
 
+@socketio.on("join_room")
+def join_chat_room(room_name):
+    join_room(room_name)
+    chat_rooms[room_name].append(session['username'])
+    emit("message", f"{session['username']} se ha unido a la sala {room_name}.", room=room_name)
+    emit("update_rooms", chat_rooms, broadcast=True, include_self=True)
 
+@socketio.on("get_last_messages")
+def get_last_messages(data):
+    room_name = data['room_name']
+    emit("last_messages", messages[room_name][-100:], include_self=True)
+
+
+@socketio.on("leave_room")
+def leave_room_function(room_name):
+    leave_room(room_name)
+    if session['username'] in chat_rooms[room_name]:
+        emit("message", f"{session['username']} ha salido de la sala {room_name}.", room=room_name)
+        chat_rooms[room_name].remove(session['username'])
+        emit("leave_room", chat_rooms, broadcast=True, include_self=True)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -14,14 +14,63 @@ document.addEventListener("DOMContentLoaded",() =>{
         NombreSala= roomName.value;
         socket.emit("create_room", NombreSala, (res) => {
             room = res;
+            document.querySelector(".title1").innerHTML = `ChatZone-${room}`;
             document.querySelector("#root").innerHTML = `Te has unido a la sala ${room}`;
             document.querySelector("#root").innerHTML += "</br>";
         });
+        const roomList = document.querySelector("#rooms-list");
+        const roomElement = document.createElement("li");
+        roomElement.innerText = roomName.value;
+        roomList.appendChild(roomElement);
+      
+        const joinButton = document.createElement("button");
+        joinButton.innerText = "Unirse";
+
+        joinButton.onclick = () => {
+          const roomName = roomElement.innerText.split(" ")[1];
+          console.log(roomName)
+          socket.emit("join_room", roomName, () => {
+            document.querySelector("#root").innerHTML = `Te has unido a la sala ${roomName}`;
+            console.log(`Unido a la sala ${roomName}`);
+          });
+          
+          // Emitir evento para solicitar los últimos 100 mensajes
+          socket.emit("get_last_messages", {room: roomName}, (messages) => {
+              // Agregar mensajes a la interfaz de usuario
+              messages.forEach((message) => {
+                  // Agregar mensaje a la interfaz de usuario
+                  document.querySelector("#root").innerHTML = ` ${message}`;
+              });
+          });
+      };
+        
+        roomElement.appendChild(joinButton);
+      
+        const leaveButton = document.createElement("button");
+        leaveButton.innerText = "Salir";
+        leaveButton.onclick = () => {
+          console.log(NombreSala)
+          socket.emit("leave_room", NombreSala, () => {
+            if (NombreSala) {
+              console.log(`Abandonó la sala ${NombreSala}`);
+              roomList.removeChild(roomElement);
+              document.querySelector(".title1").innerHTML = `ChatZone`;
+            } else {
+              console.log(`No se pudo abandonar la sala ${NombreSala}`);
+            }
+
+          });
+        };
+        roomElement.appendChild(leaveButton);
 
         const Name = document.querySelector("#room-name");
         Name.value= "";
+
     };
     
+  
+
+
     socket.on("message", (mensaje) =>{
         document.querySelector("#root").append(mensaje);
         document.querySelector("#root").innerHTML += "</br>";
@@ -31,8 +80,6 @@ document.addEventListener("DOMContentLoaded",() =>{
     
     send_message.onclick  = () =>{
         const message = document.querySelector("#input-message").value;
-        console.log(message)
-    
         socket.emit("message",{
             message,
             room: room
